77dd0e763e0c67c860d4ffc75229231dc254bf33
mskyberg <mskyberg@gmail.com>
2020-06-11 15:20:27 -0500

    implemented multiply string and made tests pass

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -7,9 +7,26 @@ Purpose: to demonstrate the use of parameters in a function
 """
 
 
-def multiply_string():
-    pass
+def multiply_string(message, n):
+    """
+    Description: this function multiplies a string
+
+    :param message: string to multiply
+    :param n: number of times to multiply string
+    :returns: new string multiplied by n
+    """
+    try:
+        new_string = message * n
+    except TypeError as e:
+        print(f'Input error: {e}')
+        raise TypeError
+    else:
+        return new_string
 
 
 if __name__ == '__main__':
-    multiply_string()
+    try:
+        print(multiply_string('Python!', 6))
+        print(multiply_string('Python!', 'Python'))
+    except TypeError:
+        print('Input Error')
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -13,6 +13,11 @@ from more_functions import string_functions as str_func
 class MyTestCase(unittest.TestCase):
     def test_multiple_string(self):
         self.assertEqual('MikeMikeMike', str_func.multiply_string('Mike', 3))
+        self.assertEqual('AaAaAaAaAaAaAaAa', str_func.multiply_string('Aa', 8))
+
+    def test_multiple_string_bad_type(self):
+        with self.assertRaises(TypeError):
+            str_func.multiply_string('Python!', 'Python!')
 
 
 if __name__ == '__main__':
