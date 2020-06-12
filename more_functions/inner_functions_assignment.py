f78af926a2301e58910686cc395417914845636e
mskyberg <mskyberg@gmail.com>
2020-06-11 19:21:19 -0500

    added logic to calculate area and perimeter for rect

--- more_functions/inner_functions_assignment.py
+++ more_functions/inner_functions_assignment.py
@@ -9,34 +9,29 @@ Purpose: demonstrate the use of inner functions
 
 def measurements(m_list):
     """
-    Use reST style.
-
+    calculate the area and perimeter of a rectangle or square
     :param m_list: list of length and width values
-    :returns: this is what is returned
-    :raises keyError: raises an exception
+    :returns: string of calculated perimeter and area
     """
 
     def area(a_list):
         """
         calculate thea area
-
         :param a_list: list of length and width values
-        :returns: this is what is returned
-        :raises keyError: raises an exception
+        :returns: calculated perimeter float
         """
-        pass
+        return a_list[0] * a_list[1]
 
     def perimeter(p_list):
         """
         calculate the perimeter
-
         :param p_list: list of length and width values
-        :returns: this is what is returned
-        :raises keyError: raises an exception
+        :returns: calculated perimeter float
         """
-        pass
+        return 2 * (p_list[0] + p_list[1])
 
-    pass
+    # "Perimeter = 11.0 Area = 7.14"
+    return f"Perimeter = {perimeter(m_list)} Area = {area(m_list)}"
 
 
 if __name__ == '__main__':
